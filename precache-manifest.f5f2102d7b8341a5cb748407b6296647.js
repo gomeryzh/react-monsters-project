a29eb462536eebf7a913c1477d3672c1cf30994c
Volodymyr Omelianiuk <ovf.1990@gmail.com>
2019-09-08 22:51:19 +0300

    Updates

---
+++ precache-manifest.f5f2102d7b8341a5cb748407b6296647.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "063a22e9e3589089a11a564c7f9e65ff",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "5cd78c63457469877849",
+    "url": "/monsters/static/css/main.d28b68d8.chunk.css"
+  },
+  {
+    "revision": "e1478644ca52e5818d0f",
+    "url": "/monsters/static/js/2.7840d41a.chunk.js"
+  },
+  {
+    "revision": "5cd78c63457469877849",
+    "url": "/monsters/static/js/main.406bbf23.chunk.js"
+  },
+  {
+    "revision": "91f8a7333514466c813f",
+    "url": "/monsters/static/js/runtime~main.5d4a3737.js"
+  }
+]);
